e8962badca3c28a44340dab7ab29b124264cfaea
Gabriele Guastella <gabrio.guastella@gmail.com>
2025-06-22 11:07:24 +0000

    Added some stl to the page. Added the toolbar with the tools buttons. Added the animation section with the buttons. Make the pencil tool work like a toggle and you can draw with the selected colour.

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const canvas = document.getElementById("pixel-canvas");
 const ctx = canvas.getContext("2d");
 const pixelSize = 16;
-
+let activeTool = null;
+let isDrawing = false;
+const pencilButton = document.getElementById("pencil-tool");
+const tools = {
+  pencil: (x, y) => {
+    ctx.fillStyle = currentColor;
+    ctx.fillRect(
+      Math.floor(x/pixelSize)*pixelSize,
+      Math.floor(y/pixelSize)*pixelSize,
+      pixelSize,
+      pixelSize
+    );
+  },
+  
+}
 function drawGrid()
 {
    
@@ -15,3 +29,53 @@ function drawGrid()
     }
 }
 drawGrid();
+
+canvas.addEventListener('mousedown', function(e) {
+    if (activeTool !== "pencil") {
+        return;
+    }
+    isDrawing = true;
+    drawingAtMouse(e);
+
+});
+
+canvas.addEventListener('mousemove', function(e) {
+    if (isDrawing && activeTool === "pencil") {
+        drawingAtMouse(e);
+    }
+});
+
+canvas.addEventListener('mouseup', function() {
+    isDrawing = false;
+});
+canvas.addEventListener('mouseleave', function() {
+    isDrawing = false;
+});
+
+pencilButton.addEventListener("click", () => {
+    if (activeTool === "pencil") {
+        alert("Pencil tool is already active.");
+    activeTool = null;
+    pencilButton.classList.remove("active"); 
+  } else {
+    alert("Pencil tool activated.");
+    activeTool = "pencil";
+    pencilButton.classList.add("active");
+  }
+});
+
+function drawingAtMouse(e)
+{
+    if (!isDrawing || activeTool !== "pencil") {
+        return;
+    }
+      const currentColor = document.getElementById("color-picker").value;
+    ctx.fillStyle = currentColor;
+    ctx.fillRect(
+    Math.floor(e.offsetX / pixelSize) * pixelSize,
+    Math.floor(e.offsetY / pixelSize) * pixelSize,
+    pixelSize,
+    pixelSize
+  );
+  
+}
